c929f9f4af2973cef4a6573cc0377f1abefb7709
yousef-in <0126268220u@gmail.com>
2022-01-12 17:15:52 -0800

    end from project

--- js/main-js-fille.js
+++ js/main-js-fille.js
@@ -1,2 +1,12 @@
 // AOS library
 AOS.init();
+
+// ========Menu=============
+var menu = document.getElementById("menu");
+function openMenu(){
+  menu.style.right = "0";
+}
+function closMenu(){
+  menu.style.right= "-200px";
+}
+//=============menu===========
